fca58f96ab4056cc8672744b1fc3337977e5ca74
subha445 <sramaviswa846@gmail.com>
2026-06-17 20:38:52 +0530

    Implement login functionality with user validation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function login() {
-alert("Login button clicked!");
+  let u = document.getElementById("user").value;
+  let p = document.getElementById("pass").value;
+
+  if (u === "admin" && p === "1234") {
+    document.getElementById("loginPage").style.display = "none";
+    document.getElementById("app").style.display = "block";
+  } else {
+    alert("Wrong login");
+  }
 }
