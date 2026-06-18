eda35a982efdcfd955a08ccb9efe8a9a3072b505
subha445 <sramaviswa846@gmail.com>
2026-06-18 20:41:06 +0530

    Add login function and remove renderTable function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function renderTable(list = students) {
-const table = document.getElementById("studentTable");
-table.innerHTML = "";
+function login() {
+let u = document.getElementById("user").value;
+let p = document.getElementById("pass").value;
 
-list.forEach((s, i) => {
-table.innerHTML += "<tr> <td>${s.name}</td> <td>${s.roll}</td> <td>${s.marks}</td> <td>${s.attendance}%</td> <td>${rank(s.marks)}</td> <td> <button onclick="editStudent(${i})">Edit</button> <button onclick="deleteStudent(${i})">Delete</button> </td> </tr>";
-});
-
-updateDashboard();
-updateChart();
+if (u === "admin" && p === "1234") {
+document.getElementById("loginPage").style.display = "none";
+document.getElementById("app").style.display = "block";
+} else {
+alert("Wrong Username or Password");
+}
 }
